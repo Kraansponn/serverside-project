3f221924ff4e18cbd5123d51f926cf0a93e5b55a
Kornel <33583478+Kraansponn@users.noreply.github.com>
2019-12-09 19:41:56 +0000

    logging in works

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3');
+
+// connects to database
+
+let db = new sqlite3.Database('database/database.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,8 +20,35 @@ router.get('/login', function(req, res, next) {
   res.render('login', {page:'login', menuId:'login'});
 });
 
+router.post('/login', function (req, res, next) {
+  var username = req.body.Username;
+  var password = req.body.password;
+  let sql = `SELECT * 
+           FROM customer 
+           WHERE username = ? AND password = ?`;
+
+  db.get(sql, [username, password], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    if (row != null) {
+      res.render('index', {page: 'index', menuId: 'index'});
+    } else {
+      res.render('register', {page: 'index', menuId: 'index'});
+    }
+  });
+});
+
 router.get('/register', function(req, res, next) {
   res.render('register', {page:'register', menuId:'register'});
+});
+
+router.get('/basket', function(req, res, next) {
+  res.render('basket', {page:'basket', menuId:'basket'});
+});
+
+router.get('/pizzabuilder', function(req, res, next) {
+  res.render('pizzabuilder', {page:'pizzabuilder', menuId:'pizzabuilder'});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var app = express();
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('database/database.db');
 
-app.get('/comments', function (request, response) {
-    console.log('get request recieved from at /comments');
-    db.all('SELECT * FROM customer', function (err, rows) {
-        if (err) {
-            console.log('error' + err);
-        } else {
-            console.log(rows);
-        }
-    })
+// var db = new sqlite3.Database('database/database.db');
+// works when in index js
+
+//
+// app.get('/comments', function (request, response) {
+//     console.log('get request recieved from at /comments');
+//     db.all('SELECT * FROM customer', function (err, rows) {
+//         if (err) {
+//             console.log('error' + err);
+//         } else {
+//             console.log(rows);
+//         }
+//     })
+// });
+
+
+
+var sqlite3 = require('sqlite3');
+
+let db = new sqlite3.Database('database/database.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+
+});
+
+// close the database connection
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Close the database connection.');
 });
